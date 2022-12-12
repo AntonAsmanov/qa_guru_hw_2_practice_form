59a6807d42dff240c57b3d800039da79256a6e1f
AntonAsmanov <antonasmanov@yandex.ru>
2022-12-12 22:43:50 +0500

    add allure steps and selenoid

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationFormPage;
@@ -29,9 +31,11 @@ public class PracticeFormTest {
 
     @BeforeAll
     static void setUp(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         //Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
